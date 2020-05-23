ba2e6b8cff779c8ebf55bbed66eb618e953d2ec2
Bolyki Balázs <bbalage0@gmail.com>
2020-05-23 22:34:25 +0200

    Learning letter frequency from source added.

--- prepper.py
+++ prepper.py
@@ -1,5 +1,30 @@
 swap_dict = {"á":"a","é":"e","í":"i","ó":"o","ö":"o","ő":"o","ú":"u","ü":"u","ű":"u"}
 accept_list = [" ","\n"]
+class UnknownLanguageException(Exception):
+  pass
+class Language:
+  def __init__(self, name, abc):
+    self.name = name
+    self.abc = list(abc)
+    self.frequency = dict()
+    for c in abc:
+      self.frequency[c] = 0
+
+  def conclude_order(self):
+    abc = []
+    for i in range(0,len(self.abc)):
+      freq = -1
+      freqlet = " "
+      for c in self.abc:
+        if self.frequency[c]>freq:
+          freq = self.frequency[c]
+          freqlet = c
+      abc.append(freqlet)
+      self.frequency[freqlet] = -1
+    self.abc = abc
+    del(self.frequency)
+    
+language_list = []
 
 def prep(sourcetext):
   text = sourcetext.lower()
@@ -18,3 +43,23 @@ def prep_from_file(sourcename):
   source = open(sourcename, "r")
   text = source.read()
   return prep(text)
+
+def init_from_default_languages():
+  language_list.clear()
+  language_list.append(Language("hun","aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz"))
+  language_list.append(Language("eng","abcdefghijklmnopqrstuvwxyz"))
+
+def learn_from_source(sourcename, language):
+  lang = None
+  for lan in language_list:
+    if lan.name == language:
+      lang = lan
+      break
+  if lang == None:
+    raise UnknownLanguageException()
+  source = open(sourcename, "r")
+  source_text = source.read()
+  for c in source_text:
+    if c in lang.abc:
+      lang.frequency[c] += 1
+  lang.conclude_order()
