5ea6b3cc8875ec395c2198daed724d74780b32f2
Bolyki Balázs <bbalage0@gmail.com>
2020-05-24 18:10:56 +0200

    Encoding now works according to language.

--- encoder.py
+++ encoder.py
@@ -1,8 +1,9 @@
 import prepper as pr
-abc_list = list("abcdefghijklmnopqrstuvwxyz")
 
-def encode_prepped_caesar(text, shift_factor):
+def encode_prepped_caesar(text, language_name, shift_factor):
   text_list = list(text)
+  lang = pr.fetch_language_by_name(language_name)
+  abc_list = lang.abc
   num = 0
   for i in range(0,len(text_list)):
     if text_list[i] in pr.accept_list:
@@ -15,10 +16,10 @@ def encode_prepped_caesar(text, shift_factor):
     text_list[i] = abc_list[num]
   return text_list
 
-def encode_caesar(text, shift_factor):
-  text_list = pr.prep(text)
-  return encode_prepped_caesar(text_list, shift_factor)
+def encode_caesar(text, language_name, shift_factor):
+  text_list = pr.prep(text, language_name)
+  return encode_prepped_caesar(text_list, language_name, shift_factor)
 
-def encode_caesar_from_file(sourcename, shift_factor):
-  text_list = pr.prep_from_file(sourcename)
-  return encode_prepped_caesar(text_list, shift_factor)
+def encode_caesar_from_file(sourcename, language_name, shift_factor):
+  text_list = pr.prep_from_file(sourcename, language_name)
+  return encode_prepped_caesar(text_list, language_name, shift_factor)
--- prepper.py
+++ prepper.py
@@ -1,4 +1,4 @@
-swap_dict = {"á":"a","é":"e","í":"i","ó":"o","ö":"o","ő":"o","ú":"u","ü":"u","ű":"u"}
+#swap_dict = {"á":"a","é":"e","í":"i","ó":"o","ö":"o","ő":"o","ú":"u","ü":"u","ű":"u"}
 accept_list = [" ","\n"]
 class UnknownLanguageException(Exception):
   pass
@@ -6,9 +6,18 @@ class Language:
   def __init__(self, name, abc):
     self.name = name
     self.abc = list(abc)
+    self.ordered_abc = None
     self.frequency = dict()
     for c in abc:
       self.frequency[c] = 0
+
+  def __str__(self):
+    abc_list = []
+    if self.ordered_abc != None:
+      abc_list = self.ordered_abc
+    retstring = self.name + "\n" + "".join(self.abc) + "\n" + "".join(abc_list)
+    del(abc_list)
+    return retstring
 
   def conclude_order(self):
     abc = []
@@ -21,42 +30,41 @@ class Language:
           freqlet = c
       abc.append(freqlet)
       self.frequency[freqlet] = -1
-    self.abc = abc
+    self.ordered_abc = abc
     del(self.frequency)
     
 language_list = []
 
-def prep(sourcetext):
+def fetch_language_by_name(language_name):
+  for lan in language_list:
+    if lan.name == language_name:
+      return lan
+  raise UnknownLanguageException()
+
+def prep(sourcetext, language_name):
+  lang = fetch_language_by_name(language_name)
   text = sourcetext.lower()
   i = 0
   text_list = list(text)
   newtext_list = []
   for c in text_list:
-    if c in swap_dict:
-      text_list[i]=swap_dict[c]
-    if c.isalpha() or c in accept_list:
+    if c in lang.abc or c in accept_list:
       newtext_list.append(text_list[i])
     i+=1
   return newtext_list
 
-def prep_from_file(sourcename):
+def prep_from_file(sourcename, language_name):
   source = open(sourcename, "r")
   text = source.read()
-  return prep(text)
+  return prep(text, language_name)
 
 def init_from_default_languages():
   language_list.clear()
   language_list.append(Language("hun","aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz"))
   language_list.append(Language("eng","abcdefghijklmnopqrstuvwxyz"))
 
-def learn_from_source(sourcename, language):
-  lang = None
-  for lan in language_list:
-    if lan.name == language:
-      lang = lan
-      break
-  if lang == None:
-    raise UnknownLanguageException()
+def learn_from_source(sourcename, language_name):
+  lang = fetch_language_by_name(language_name)
   source = open(sourcename, "r")
   source_text = source.read()
   for c in source_text:
